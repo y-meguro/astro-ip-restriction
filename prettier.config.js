21004c5eb946e883291bc5e8dd8e865321643760
Yohei <y.meguro.2008@gmail.com>
2023-12-04 18:04:02 +0900

    first commit

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ["prettier-plugin-astro"],
+  singleQuote: true,
+  semi: false,
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+}
